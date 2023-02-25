eb6beb85dd694b32e4ffb77cbc0a663ea0f50791
Crisar2001 <88943646+Crisar2001@users.noreply.github.com>
2023-02-25 11:10:14 -0600

    Practica 3

--- imprimir.js
+++ imprimir.js
@@ -1,12 +1,48 @@
-//arcvo de java script
+// var campoTexto = document.getElementById("campoTexto")
 
-var campoTexto=document.getElementById("campoTexto");
-console.log(campoTexto);
+// console.log(campoTexto);
 
-function imprimir(){
-    console.log(campoTexto.value);
+// function imprimir ()
+// {
+//     console.log(campoTexto.value);
+// }
+
+// //Función para limpiar la caja de texto
+// function limpiar()
+// {
+//     //console.clear(campoTexto.value); PARA LIMPIAR TODA LA CONSOLA (COMO ELIMINARLA)
+//     campoTexto.value="";
+// }
+
+let usuarioTXT = document.getElementById("Usuario");
+let passwordTXT = document.getElementById("password");
+let p = document.getElementById("mensaje");
+
+function login(){
+    //"Iniciaste sesión correctamente"
+    p.innerHTML = "Login correcto, " + usuarioTXT.value;
+
+    limpiar();
+}
+
+function registro(){
+    //Registraste la sesión correctamente
+    p.innerHTML = "Registro completo, " + usuarioTXT.value;
+
+    limpiar();
 }
 
 function limpiar(){
-    console.clear(campoTexto);
+    usuarioTXT.value = "";
+    passwordTXT.value = "";
 }
+
+function tooglePsw(){
+    password.value= Text;
+}
+
+/*function tooglePssw()
+{
+    1. CAMBIAR EL ATRIBUTO TYPE
+    2. CAMBIA EL TEXTO DE SPAN
+}*/
