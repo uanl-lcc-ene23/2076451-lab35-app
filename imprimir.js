cb0e53002b62da057d15bc958eac3a50ea31cf4e
Crisar2001 <88943646+Crisar2001@users.noreply.github.com>
2023-06-05 18:56:44 -0500

    practica7

--- imprimir.js
+++ imprimir.js
@@ -1,22 +1,84 @@
-<ion-app>
-<ion-header>
-  <ion-toolbar>
-    <ion-title>Presupuesto</ion-title>
-  </ion-toolbar>
-</ion-header>
+const listaGastos = [];
+const tipoGastoInput = document.getElementById("tipo-gasto");
+const montoGastoInput = document.getElementById("monto-gasto");
+const guardarGastoButton = document.getElementById("guardar-gasto");
+const listaGastosElement = document.getElementById("lista-gastos");
 
-<ion-content class="ion-padding">
-    <ion-label>Monto:</ion-label>
-    <ion-input type="Text" id="Monto"></ion-input>
+function agregarGasto() {
+  const tipoGasto = tipoGastoInput.value;
+  const montoGasto = parseFloat(montoGastoInput.value);
+  if (tipoGasto.trim() !== "" && !isNaN(montoGasto)) {
+    const gasto = {
+      tipo: tipoGasto,
+      monto: montoGasto
+    };
+    listaGastos.push(gasto);
+    actualizarListaGastos();
+    tipoGastoInput.value = "";
+    montoGastoInput.value = "";
+  } else {
+    alert("Ingrese un tipo de gasto y un monto válido.");
+  }
+}
 
-    <div class="fila">
-        <ion-label>Monto:</ion-label>
-        <ion-input type="Text" id="Monto"></ion-input>
-    </div>
+function actualizarListaGastos() {
+  listaGastosElement.innerHTML = "";
+  listaGastos.forEach((gasto) => {
+    const item = document.createElement("ion-item");
+    item.textContent = `${gasto.tipo}: $${gasto.monto.toFixed(2)}`;
+    listaGastosElement.appendChild(item);
+  });
+}
 
-    <div class="fila">
-        <ion-button onclick="guardar">Guardar</ion-button>
-    </div>
-   //<h1>Main Content</h1>
-</ion-content>
-</ion-app>
+guardarGastoButton.addEventListener("click", agregarGasto);
+
+// Agregar menu lateral
+const menuLateral = document.createElement("ion-menu");
+menuLateral.setAttribute("side", "start");
+menuLateral.setAttribute("menu-id", "menu-lateral");
+
+// Agregar encabezado
+const encabezadoMenuLateral = document.createElement("ion-header");
+const toolbarMenuLateral = document.createElement("ion-toolbar");
+toolbarMenuLateral.setAttribute("color", "primary");
+const tituloMenuLateral = document.createElement("ion-title");
+tituloMenuLateral.textContent = "Hola Menu";
+toolbarMenuLateral.appendChild(tituloMenuLateral);
+encabezadoMenuLateral.appendChild(toolbarMenuLateral);
+menuLateral.appendChild(encabezadoMenuLateral);
+
+// Agregar contenido
+const contenidoMenuLateral = document.createElement("ion-content");
+const listaOpcionesMenuLateral = document.createElement("ion-list");
+const opcion1MenuLateral = document.createElement("ion-item");
+opcion1MenuLateral.innerHTML = "<ion-icon name='settings'></ion-icon>Opción 1";
+const opcion2MenuLateral = document.createElement("ion-item");
+opcion2MenuLateral.innerHTML = "<ion-icon name='information-circle'></ion-icon>Opción 2";
+listaOpcionesMenuLateral.appendChild(opcion1MenuLateral);
+listaOpcionesMenuLateral.appendChild(opcion2MenuLateral);
+contenidoMenuLateral.appendChild(listaOpcionesMenuLateral);
+
+// Agregar botón "Volver al inicio"
+const botonVolverInicio = document.createElement("ion-button");
+botonVolverInicio.setAttribute("fill", "clear");
+botonVolverInicio.setAttribute("expand", "full");
+botonVolverInicio.setAttribute("routerLink", "/");
+botonVolverInicio.innerHTML = "<ion-icon name='arrow-back'></ion-icon>Volver al inicio";
+contenidoMenuLateral.appendChild(botonVolverInicio);
+
+const comentarioMenuLateral = document.createElement("div");
+comentarioMenuLateral.setAttribute("style", "font-size: 0.75rem; text-align: center; margin-top: 8px;");
+comentarioMenuLateral.textContent = "Menu principal";
+contenidoMenuLateral.appendChild(comentarioMenuLateral);
+  
+const menuButton = document.createElement("ion-button");
+menuButton.setAttribute("slot", "start");
+menuButton.setAttribute("menu-toggle", "");
+menuButton.innerHTML = "<ion-icon name='menu'></ion-icon>";
+toolbarMenuLateral.appendChild(menuButton);
+
+const volverInicioButton = document.getElementById("volver-inicio");
+volverInicioButton.addEventListener("click", () => {
+  const menu = document.querySelector("ion-menu");
+  menu.close();
+});
